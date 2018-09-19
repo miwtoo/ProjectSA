b25148000aa8b5c06d42c91825cd60178411b4bc
Miwtoo-NB <miwyugioh@gmail.com>
2018-09-19 23:42:34 +0700

    add findById

--- server/src/main/java/com/example/demo/controller/TakeInController.java
+++ server/src/main/java/com/example/demo/controller/TakeInController.java
@@ -1,11 +1,14 @@
 package com.example.demo.controller;
 
 import java.util.Collection;
+import java.util.Optional;
 
 import com.example.demo.entity.TakeIn;
 import com.example.demo.repository.TakeInRepository;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,5 +22,15 @@ public class TakeInController{
     @GetMapping("/take-in")
     public Collection<TakeIn> takeinAll(){
         return repo.findAll();
+    }
+
+    @GetMapping("/take-in/{id}")
+    public Optional<TakeIn> takeinByid(@PathVariable Long id ){
+        return repo.findById(id);
+    }
+
+    @PostMapping("/take-in/")
+    public void AddItem(){
+        
     }
 }
