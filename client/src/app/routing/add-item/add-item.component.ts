ae97e26f09c0b9583c0cf3a9be9f6d86bd9f7166
Miwtoo-NB <miwyugioh@gmail.com>
2018-09-22 14:59:26 +0700

    fix head

--- client/src/app/routing/add-item/add-item.component.ts
+++ client/src/app/routing/add-item/add-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {HttpHeaders} from '@angular/common/http';
+import {HttpClient,HttpParams } from '@angular/common/http';
 @Component({
   selector: 'app-add-item',
   templateUrl: './add-item.component.html',
@@ -7,9 +8,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddItemComponent implements OnInit {
 
-  constructor() { }
+  typelists = [];
+
+  onClickSubmit(data) {
+
+    const body = new HttpParams()
+    .set('item_name', data.item_name.toString())
+    .set('type.type_id', data.type_id.toString())
+    .set('price', data.price.toString())
+    .set('amount', data.amount.toString())
+
+    console.log(body.toString());
+
+    this.http.post("http://localhost:8080/item",body.toString(),{headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')}).subscribe(
+      data => {
+        console.log("POST Request is successful ", data);
+      },
+      error => {
+        console.log("Error", error);
+      }
+
+    );
+    
+  }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.typelists.push(
+      
+    )
+
+    this.http.get("http://localhost:8080/typelist").subscribe(
+      data => {
+        console.log("GET Request is successful ", data);
+        for (let index = 0; index < data["length"]; index++) {
+          this.typelists.push({
+            value: data[index].type_id, 
+            viewValue: data[index].type
+          })
+
+
+        }
+        //console.log(ELEMENT_DATA);
+      },
+      error => {
+        console.log("Error", error);
+      }
+
+    );
   }
 
 }
