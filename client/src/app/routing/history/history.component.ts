2a42896c9c5627c29cef793028299a0c7d66def0
Miwtoo-NB <miwyugioh@gmail.com>
2018-09-22 14:08:02 +0700

    get list

--- client/src/app/routing/history/history.component.ts
+++ client/src/app/routing/history/history.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {HttpClient } from '@angular/common/http';
 export interface PeriodicElement {
   name: string;
   item_id:string;
@@ -7,13 +7,6 @@ export interface PeriodicElement {
   time: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {name: "นาย วงศกร ชูเกษม", item_id: "PSU004", amount: 5, time: "5 สิงหาคม 2561, 10:20 น."},
-  {name: "นาย ศรัญยู เงาศรี", item_id: "ZXP003", amount: 10, time: "5 สิงหาคม 2561, 16:00 น."},
-  {name: "นาย ศรัญยู เงาศรี", item_id: "IPX002", amount: 5, time: "5 สิงหาคม 2561, 16:10 น."},
-  {name: "นาย วงศกร ชูเกษม", item_id: "PSU001", amount: 5, time: "6 สิงหาคม 2561, 10:00 น."},
-  {name: "นาย ชาญชัย นนทะชัย", item_id: "ZXP003", amount: 10, time: "6 สิงหาคม 2561, 16:00 น."},
-];
 @Component({
   selector: 'app-history',
   templateUrl: './history.component.html',
@@ -22,11 +15,34 @@ const ELEMENT_DATA: PeriodicElement[] = [
 export class HistoryComponent implements OnInit {
 
   displayedColumns: string[] = ['name', 'item_id', 'amount', 'time'];
-  dataSource = ELEMENT_DATA;
+  dataSource;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    const ELEMENT_DATA: PeriodicElement[] = [];
+    
+    this.http.get("http://localhost:8080/withdraw").subscribe(
+      data => {
+        console.log("GET Request is successful ", data);
+        for (let index = 0; index < data["length"]; index++) {
+          ELEMENT_DATA.push({
+            name: data[index].user_id.user_name,
+            item_id: data[index].item_id.item_name,
+            amount: data[index].amount,
+            time: data[index].time
+          })
+
+
+        }
+        //console.log(ELEMENT_DATA);
+        this.dataSource = ELEMENT_DATA;
+      },
+      error => {
+        console.log("Error", error);
+      }
+
+    );
   }
 
 }
--- client/src/app/routing/item-list/item-list.component.ts
+++ client/src/app/routing/item-list/item-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {MatTableDataSource} from '@angular/material';
+import {HttpClient } from '@angular/common/http';
 
 export interface PeriodicElement {
   id: string;
@@ -9,29 +10,56 @@ export interface PeriodicElement {
   amount: number;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {id: "IPX001", name: "* SET PROVAMED SUN SPF 50+ FACE สี BEIGE (FREE SUN PERFECAT CLEANSING WATER 50ML.)", type: "เวชสำอาง/ครีมบำรุงผิว", price: 100, amount: 100},
-  {id: "IPX002", name: "ACNE-AID 100GM ก้อน***รุ่นใหม่ [GSK/STEFEL]", type: "เวชสำอาง/ครีมบำรุงผิว", price: 180, amount: 30},
-  {id: "ZXP003", name: "AMSEL ACEROLA CHERRY PLUS 1200 MG 10SAC", type: "ผลิตภัณฑ์เสริมอาหาร", price: 180, amount: 20},
-  {id: "PSU004", name: "*KACHANA (กระชายดำสกัด+สมุนไพรบำรุงผู้ชาย) 3*10's[BKD]กล่องรุ่นใหม่", type: " สมุนไพรยาแผนโบราณ", price: 850, amount: 10},
-];
+
 @Component({
   selector: 'app-item-list',
   templateUrl: './item-list.component.html',
   styleUrls: ['./item-list.component.css']
 })
 export class ItemListComponent implements OnInit {
+   
 
   displayedColumns: string[] = ['id', 'name', 'type', 'price','amount'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  dataSource = new MatTableDataSource();
+
+ 
+    
+  
 
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+
+    const ELEMENT_DATA: PeriodicElement[] = [];
+    
+    this.http.get("http://localhost:8080/item").subscribe(
+      data => {
+        console.log("GET Request is successful ", data);
+        for (let index = 0; index < data["length"]; index++) {
+          ELEMENT_DATA.push({
+            id: data[index].item_id,
+            name: data[index].item_name,
+            type: data[index].type.type,
+            price: data[index].price,
+            amount: data[index].amount
+          })
+
+
+        }
+        //console.log(ELEMENT_DATA);
+        this.dataSource = new MatTableDataSource(ELEMENT_DATA);
+      },
+      error => {
+        console.log("Error", error);
+      }
+
+    );
+
+    
   }
 
 }
